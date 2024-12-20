1245c665a2b3ea048ce8e8c59bc4048e6e4dba11
saraiwr <117382906+sara11211@users.noreply.github.com>
2024-12-20 10:34:58 +0100

    styles added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-blue': '#0F60FF', 
+        'black': '#23272E', 
+        'accent-blue': '#F4F7FE', 
+        'dark-gray': '8B909A',
+        'background-gray': 'FAFAFA',
+        'white' : 'FFFFFF',
+      },
+      fontFamily: {
+        'main': ['Public Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
