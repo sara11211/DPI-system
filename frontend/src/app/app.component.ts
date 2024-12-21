3012850b83409cc9eb269e84984122e8e5e0c2e2
saraiwr <117382906+sara11211@users.noreply.github.com>
2024-12-21 15:44:41 +0100

    Dashboard Sidebar Done

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/utilities/button/button.component';
+import { SidebarComponent } from './components/utilities/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, ButtonComponent, SidebarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+  text = signal('Sauvegarder');
 }
---
+++ frontend/src/app/components/utilities/sidebar/sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {
+}
