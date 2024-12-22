177065205a500a2783abb58d097fe197c2386f46
saraiwr <117382906+sara11211@users.noreply.github.com>
2024-12-22 23:01:51 +0100

    small fix

---
+++ frontend/src/app/dashboards/dashboard-administratif/pages/modifier-dossier/modifier-dossier.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modifier-dossier',
+  standalone: true,
+  imports: [],
+  templateUrl: './modifier-dossier.component.html',
+  styleUrl: './modifier-dossier.component.css'
+})
+export class ModifierDossierComponent {
+
+}
