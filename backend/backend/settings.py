786501815920235c1922db92a68732f27003c9d8
afafboukhari <afafboukhari04@gmail.com>
2024-12-26 13:17:56 +0100

    necessary models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework' ,
-     'consultation_crud'
+     'consultation_crud',
+     'bilanbiologique',
 ]
 
 MIDDLEWARE = [
