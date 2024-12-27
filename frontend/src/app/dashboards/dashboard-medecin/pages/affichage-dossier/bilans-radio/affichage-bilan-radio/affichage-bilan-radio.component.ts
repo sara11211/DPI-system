865ef4e3db3f1c85c17d15366ce2f917931525b9
saraiwr <117382906+sara11211@users.noreply.github.com>
2024-12-27 18:57:43 +0100

    affichage bilan radio

--- frontend/src/app/dashboards/dashboard-medecin/pages/affichage-dossier/bilans-radio/affichage-bilan-radio/affichage-bilan-radio.component.ts
+++ frontend/src/app/dashboards/dashboard-medecin/pages/affichage-dossier/bilans-radio/affichage-bilan-radio/affichage-bilan-radio.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-affichage-bilan-radio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './affichage-bilan-radio.component.html',
   styleUrl: './affichage-bilan-radio.component.css'
 })
@@ -15,4 +17,9 @@ export class AffichageBilanRadioComponent implements OnInit {
     // Retrieve the consultation ID from the route parameters
     const consultationId = this.route.snapshot.paramMap.get('id');
   } 
+
+  symptoms: string = 'Exemple de symptômes déjà renseignés';
+  diagnostic: string = 'Exemple de diagnostic déjà renseigné';
+  measuresInfo: string = 'Exemple des informations sur les mesures déjà renseignées';
+  nextConsultation: string = '2024-12-31';
 }
