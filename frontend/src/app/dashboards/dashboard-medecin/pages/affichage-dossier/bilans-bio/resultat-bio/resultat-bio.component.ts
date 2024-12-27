41a1daeba2bef2487c3d238604e064eee6f76400
saraiwr <117382906+sara11211@users.noreply.github.com>
2024-12-27 17:29:29 +0100

    resultat bilan biologique

--- frontend/src/app/dashboards/dashboard-medecin/pages/affichage-dossier/bilans-bio/resultat-bio/resultat-bio.component.ts
+++ frontend/src/app/dashboards/dashboard-medecin/pages/affichage-dossier/bilans-bio/resultat-bio/resultat-bio.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-resultat-bio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './resultat-bio.component.html',
   styleUrl: './resultat-bio.component.css',
 })
@@ -15,4 +16,11 @@ export class ResultatBioComponent implements OnInit {
     // Retrieve the consultation ID from the route parameters
     const consultationId = this.route.snapshot.paramMap.get('id');
   }
+
+  resultats = [
+    { mesure: 'Glucose', value: '100 mg/dL' },
+    { mesure: 'Hemoglobin', value: '13.5 g/dL' },
+    { mesure: 'Cholesterol', value: '180 mg/dL' },
+    { mesure: 'Calcium', value: '9.8 mg/dL' },
+  ];
 }
