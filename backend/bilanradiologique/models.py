942ba28601cc77ecfcae568841f706b8eba5275a
afafboukhari <afafboukhari04@gmail.com>
2024-12-30 01:34:37 +0100

    crud bilan radiologique

--- backend/bilanradiologique/models.py
+++ backend/bilanradiologique/models.py
@@ -1,3 +1,92 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Personnel(models.Model):
+    nom = models.CharField(max_length=255, blank=True, null=True)
+    prenom = models.CharField(max_length=255, blank=True, null=True)
+    fonction = models.CharField(max_length=23, blank=True, null=True)
+    users = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True, related_name='personnel_users')
+
+    class Meta:
+        managed = False
+        db_table = 'personnel'
+
+
+class Dpis(models.Model):
+    nss = models.BigIntegerField(blank=True, null=True)
+    nom = models.CharField(max_length=255, blank=True, null=True)
+    prenom = models.CharField(max_length=255, blank=True, null=True)
+    date_naissance = models.DateField(blank=True, null=True)
+    adresse = models.CharField(max_length=255, blank=True, null=True)
+    num_telephone = models.CharField(max_length=255, blank=True, null=True)
+    medecins = models.ForeignKey('Medecins', models.DO_NOTHING, blank=True, null=True)
+    personnes_contact = models.ForeignKey('PersonnesContact', models.DO_NOTHING, blank=True, null=True)
+    qr_url = models.CharField(max_length=255, blank=True, null=True)
+    antecedents = models.CharField(max_length=255, blank=True, null=True)
+    users = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True, related_name='dpis_users')
+
+    class Meta:
+        managed = False
+        db_table = 'dpis'
+
+
+class BilansRadiologiques(models.Model):
+    synthese_bilan_radio = models.TextField(blank=True, null=True)
+    date_radiologie = models.DateField(blank=True, null=True)
+    type_radiologie = models.CharField(max_length=255, blank=True, null=True)
+    image_url = models.CharField(max_length=255, blank=True, null=True)
+    radiologues = models.ForeignKey('Radiologues', models.DO_NOTHING, blank=True, null=True)
+    consultations = models.ForeignKey('Consultations', models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'bilans_radiologiques'
+
+
+class Medecins(models.Model):
+    specialite = models.CharField(max_length=255, blank=True, null=True)
+    personnel = models.ForeignKey('Personnel', models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'medecins'
+
+class ResumesConsultations(models.Model):
+    diagnostic = models.TextField(blank=True, null=True)
+    symptomes = models.TextField(blank=True, null=True)
+    mesure = models.TextField(blank=True, null=True)
+    antecedents = models.TextField(blank=True, null=True)
+    info_supp = models.TextField(blank=True, null=True)
+    date_prochaine_consultation = models.DateField(blank=True, null=True)
+    dpis_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'resumes_consultations'
+        
+class Consultations(models.Model):
+    dpis = models.ForeignKey('Dpis', models.DO_NOTHING, blank=True, null=True)
+    resume_consultation = models.ForeignKey('ResumesConsultations', models.DO_NOTHING, blank=True, null=True)
+    date_consultation = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'consultations'
+
+
+class PersonnesContact(models.Model):
+    nom_contact = models.CharField(max_length=255, blank=True, null=True)
+    prenom_contact = models.CharField(max_length=255, blank=True, null=True)
+    numero_telephone = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'personnes_contact'
+
+
+class Radiologues(models.Model):
+    personnel = models.ForeignKey(Personnel, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'radiologues'
--- backend/bilanradiologique/views.py
+++ backend/bilanradiologique/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
+from .models import BilansRadiologiques,Medecins,ResumesConsultations,Radiologues,Consultations,Personnel,PersonnesContact
+from .serializers import BilansRadiologiquesSerializer,MedecinsSerializer,ResumesConsultationsSerializer,RadiologuesSerializer,Consultations,PersonnelSerializer,PersonnesContactSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+#.#################################
+#_fonctions de bilan Radiologique  #
+#.################################
+
+class bilan_radio(APIView):
+    def get(self, request):
+        bilans = BilansRadiologiques.objects.filter(date_radiologie__isnull=True) #exams not yet done
+        serializer = BilansRadiologiquesSerializer(bilans, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)  
+    
+    def post(self, request):
+        serializer = BilansRadiologiquesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        try:
+            bilan = BilansRadiologiques.objects.get(pk=pk)
+        except BilansRadiologiques.DoesNotExist:
+            return Response({"error": "Object not found."}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = BilansRadiologiquesSerializer(bilan, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        try:
+            bilan = BilansRadiologiques.objects.get(pk=pk)
+        except BilansRadiologiques.DoesNotExist:
+            return Response({"error": "Object not found"}, status=status.HTTP_404_NOT_FOUND)
+        bilan.delete()
+        return Response({"message": "Object deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    
+#afficher les bilans d'un patient donné :
+class bilan_patient(APIView):
+    def get(self, request, patient_id):
+        consultations = Consultations.objects.filter(dpis=patient_id)
+        bilans = BilansRadiologiques.objects.filter(consultations__in=consultations)
+        
+        if bilans.exists():
+            serializer = BilansRadiologiquesSerializer(bilans, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": "Ce patient n'a effectué aucun bilan Radiologique"}, status=status.HTTP_404_NOT_FOUND)
