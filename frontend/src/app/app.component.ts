3806599a820393d72d8becd494fa5a9e8b241bda
saraiwr <117382906+sara11211@users.noreply.github.com>
2025-01-03 20:45:16 +0100

    loading page added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './dashboards/dashboard-medecin/sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
 import { DashboardRouteService } from './app.routes';
+import { LoadingPageComponent } from './components/loading-page/loading-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, SidebarComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, SidebarComponent, LoadingPageComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
